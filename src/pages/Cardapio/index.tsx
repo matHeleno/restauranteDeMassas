6df493f4a294b6aef4f0f02e2449eba65ec9e1a4
matHeleno <matheuslopesheleno@gmail.com>
2022-07-10 13:15:20 -0300

    Termino da primeira aula

---
+++ src/pages/Cardapio/index.tsx
@@ -0,0 +1,11 @@
+import styles from './Cardapio.module.css';
+
+export default function Cardapio() {
+    return (
+        <main>
+            <nav className="menu">
+                
+            </nav>
+        </main>
+    )
+}
